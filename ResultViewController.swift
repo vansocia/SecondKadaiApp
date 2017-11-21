55ccf3424e2fc3ca5505a06db3f019dce1e64658
vansocia <k_sakurai@vansocia.com>
2017-11-21 17:35:25 +0900

    result view コントローラーを削除して作り直した。

--- ResultViewController.swift
+++ ResultViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ResutlViewController.swift
+//  ResultViewController.swift
 //  SecondKadaiApp
 //
 //  Created by 櫻井 謙充 on 2017/11/21.
@@ -9,7 +9,7 @@
 import UIKit
 
 class ResutlViewController: UIViewController {
-
+    
     // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
     @IBOutlet weak var label: UILabel!
     
@@ -19,26 +19,26 @@ class ResutlViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         let result = x
         label.text = "こんにちは \(result) さん"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
